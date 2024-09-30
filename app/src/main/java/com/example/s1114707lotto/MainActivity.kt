a2cfb0e26a9b621064cfcdcd36549e6d2dc0b323
許皓翔 <xuxiang0707@gmail.com>
2024-09-30 15:55:57 +0800

    加入樂透功能

--- app/src/main/java/com/example/s1114707lotto/MainActivity.kt
+++ app/src/main/java/com/example/s1114707lotto/MainActivity.kt
@@ -1,24 +1,26 @@
 package com.example.s1114707lotto
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        var txv: TextView = findViewById(R.id.txv)
-        txv.text = "0"
 
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            var txv: TextView = findViewById(R.id.txv)
+            txv.text = "0"
+        }
+
+        fun happy(v: View){
+            var txv: TextView = findViewById(R.id.txv)
+            txv.text = (1..100).random().toString()
+        }
     }
-}
+
